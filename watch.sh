841f9125e4012103f3aa0198bf3c1eee283f3b99
Gur Telem <gur111telem@gmail.com>
2021-01-19 12:14:49 +0200

    Add watch for ex5

--- watch.sh
+++ watch.sh
@@ -4,13 +4,13 @@ GREEN='[0;32m'
 ORANGE='[0;33m'
 NC='[0m'
 
-fswatch -0 ./mlist_ex5.sml | while read -d "" event 
+watch -n 5 | while read -d "" event 
 do 
     clear
-    cat mlisp.sml hw3_q3.sml mlist_ex5.sml > tmp.sml
-    sml tmp.sml | grep -A 50 --color=always here \
+#    cat mlisp.sml hw3_q3.sml mlisp_ex5.sml > tmp.sml
+# | grep -A 50 --color=always here \
+    sml < eval_tests.sml \
 		| sed -e "s/true/${esc}${GREEN}&${esc}${NC}/g" \
 		| sed -e "s/bool/${esc}${ORANGE}&${esc}${NC}/g" \
 		| sed -e "s/false/${esc}${RED}&${esc}${NC}/g"
-
 done
