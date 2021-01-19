1aad970918a2ffa9c2661b4fd72b7dc871087928
Gur Telem <gur111telem@gmail.com>
2021-01-19 20:17:51 +0200

    Finish Q2 Wet6

--- HW6.pl
+++ HW6.pl
@@ -7,7 +7,7 @@
 %:- use_module(library(clpq)).  % Constraints over rational numbers
 
 % Your program goes here
-/**Q1*/
+/**Q1.1*/
 card(number,suit).
 card(X,Y):- X #>= 2, X #=< 14, (Y ='clubs';Y ='hearts';Y ='spades';Y ='diamonds').
 x_smaller_y(X,Y):- 
@@ -42,7 +42,7 @@ lowest(X,Res) :-
     find_smallest(X, card(14,diamonds),Res).
 
 
-/**Q2*/
+/**Q1.2*/
 
 bigger(card(X,_),card(H_X,_)) :-
     H_X>X.
@@ -56,13 +56,13 @@ filter_t(card(P1,P2),[X|Xs],[X|Ys]) :-
     bigger(card(P1,P2),X),
     filter_t(card(P1,P2),Xs, Ys).
 
-filter_t(card(P1,P2),[X|Xs],Ys) :-
+filter_t(card(P1,P2),[_|Xs],Ys) :-
     filter_t(card(P1,P2),Xs, Ys).
 
 filter(card(P1,P2),[H|T],Res) :- 
     filter_t(card(P1,P2),[H|T],Res).
 
-/**Q3*/
+/**Q1.3*/
 x_smaller_y_w(X,Y):- 
     Y='diamonds';
     (Y='spades',(X='clubs';X='hearts';X='spades'));
@@ -83,7 +83,7 @@ bigger_winner(card(X,XS),card(Y,YS),Res) :-
     Y=X,
     x_smaller_y_w(XS,YS),
     Res = 1.
-bigger_winner(card(X,XS),card(Y,YS),Res) :-
+bigger_winner(card(X,_),card(Y,_),Res) :-
     Y=X,
     Res = 2.
 
@@ -93,3 +93,47 @@ winner([X|XS],[Y|YS],card(P1,P2),Res) :-
     lowest(XRes,Xlowest),
     lowest(YRes,Ylowest),
     bigger_winner(Xlowest,Ylowest,Res).
+
+/**Q2.1*/
+
+replace_t(Head,[_|Tail],N,New,Res) :-
+    N #= 0,
+    append(Head, [New], T),
+    append(T,Tail,Res).
+
+replace_t(Head,[X|Tail], N, New, Res) :-
+    N#>0,
+    append(Head,[X],T),
+    replace_t(T,Tail,N-1,New,Res).
+
+replace([_|Tail], N, New, Res) :-
+    N#=0,
+    Res=[New|Tail].
+
+replace([L|Tail], N, New, Res) :-
+    N#>0,
+    replace_t([L], Tail,N-1,New,Res).
+
+
+/**Q2.2*/
+
+turing(State, Actions, tape(Data, Index), Res) :-
+    replace(Data, Index, X, Data),
+    replace(Actions, _, action(State, _, X, Y, h), Actions),
+    replace(Data, Index, Y, NData),
+    Index #= N,
+    Res=tape(NData, N).
+
+turing(State, Actions, tape(Data, Index), Res) :-
+    replace(Data, Index, X, Data),
+    replace(Actions, _, action(State, NState, X, Y, l), Actions),
+    replace(Data, Index, Y, NData),
+    turing(NState, Actions, tape(NData, Index-1), Res).
+
+turing(State, Actions, tape(Data, Index), Res) :-
+    replace(Data, Index, X, Data),
+    replace(Actions, _, action(State, NState, X, Y, r), Actions),
+    replace(Data, Index, Y, NData),
+    turing(NState, Actions, tape(NData, Index+1), Res).
+
+/**Queries*/
